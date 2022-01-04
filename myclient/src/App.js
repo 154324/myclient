4cf9c31f001dc7c931102875468e3eef761e8248
154324 <80805198+154324@users.noreply.github.com>
2022-01-04 22:52:59 +0900

    create customer

--- myclient/src/App.js
+++ myclient/src/App.js
@@ -1,24 +1,52 @@
-import './App.css';
+
 import Customer from './components/Customer';
+
+
 function App() {
 
-
-  const customer = {
+  const customer = [{
     id : 1,
+    img : "http://placeimg.com/64/64/1",
     name: "나동빈",
     birthday: "961221",
     gender: '남자',
     job : "대학생"
-  }
+  },
+    {id : 2,
+    img : "http://placeimg.com/64/64/2",
+    name: "박경우",
+    birthday: "961027",
+    gender: '남자',
+    job : "대학생"
+  },
+  {id : 3,
+    img : "http://placeimg.com/64/64/3",
+    name: "김정원",
+    birthday: "981223",
+    gender: '여자',
+    job : "대학생"
+  },
+]
+
 
   return (
     <div className="App">
-
       <h2>고객관리 프로그램 만들기</h2>
-      <Customer/>
-      
- 
-
+      {
+        customer.map(c=>{
+          return(
+            <Customer
+            key={c.id}
+            id={c.id}
+            img={c.img}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+          )
+        })
+      }
     </div>
   );
 }
--- myclient/src/components/Customer.js
+++ myclient/src/components/Customer.js
@@ -1,8 +1,12 @@
-export default function Customer(){
+
+export default function Customer(props){
     return (
         <div>
-            <h2>진짜 돼?
-            </h2>
+            <h2>({props.id}) {props.name}</h2>
+            <p><img src={props.img} alt="profile"></img></p>
+            <p>{props.birthday}</p>
+            <p>{props.gender}</p>
+            <p>{props.job}</p>
         </div>
     )
 }
